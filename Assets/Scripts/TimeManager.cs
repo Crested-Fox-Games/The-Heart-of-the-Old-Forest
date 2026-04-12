ac62dd240575e622580ddb1a1a702edde4936929
Raxadore <liam@nellakir.com.au>
2026-04-12 14:00:59 +1000

    Got time functional
    Fixed the calculations to correctly use angles to move the directional light.
    
    Moving the directional light updates the skybox so thats working too.

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -10,15 +10,13 @@ public class TimeManager : MonoBehaviour
     private float cycleTime = 0f;
     private float cycleDuration = 720f; // 12 minutes in seconds
     private float cycleDayDuration = 480f; // 8 minutes in seconds
-    private float dayPercent; //The percentage of time that the day covers
-
+    private float sunAngle = 0; // Used in calculating sun position
     private int currentDay = 0;
 
     private void Start()
     {
         lightRotation = sceneLight.transform.rotation;
         lightRotation.x = 0;
-        dayPercent = 100 / cycleDuration * cycleDayDuration; //Should be around 66%
     }
 
     // Update is called once per frame
@@ -47,14 +45,17 @@ public class TimeManager : MonoBehaviour
         if(cycleTime < cycleDayDuration)
         {
             //Day period
-            lightRotation.x += 180 / cycleDayDuration * Time.deltaTime;
+            sunAngle += 180f / cycleDayDuration * Time.deltaTime;
         }
         else
         {
             //Night period
-            lightRotation.x += 180 / (cycleDuration - cycleDayDuration) * Time.deltaTime;
+            sunAngle += 180f / (cycleDuration - cycleDayDuration) * Time.deltaTime;
         }
 
-        sceneLight.transform.rotation = lightRotation;
+        if (sunAngle >= 360f)
+            sunAngle -= 360f;
+
+        sceneLight.transform.localRotation = Quaternion.Euler(sunAngle, lightRotation.y, lightRotation.z);
     }
 }
