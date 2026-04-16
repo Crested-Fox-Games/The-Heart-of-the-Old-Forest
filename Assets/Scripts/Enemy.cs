b6c61afa58cd82ed9303dbf5463b5b6393a0f4b4
Raxadore <liam@nellakir.com.au>
2026-04-16 17:29:54 +1000

    Added value initialization for enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil;
 using UnityEngine;
 
 //TODO: make this class an abstract parent when implementing enemy types
@@ -7,13 +8,40 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private EnemySO enemySO;
 
+    private string enemyName, enemyDescription;
+    private float enemyHealth, enemySpeed, enemyDamage;
+
     #endregion
 
-    float health = 5;
+    private float currentHealth ;
+
+    private void Start()
+    {
+        InitializeValues();
+        currentHealth = enemyHealth;
+    }
+
+    /// <summary>
+    /// Sets the initial values of the node based on the SO
+    /// </summary>
+    private void InitializeValues()
+    {
+        //Strings
+        enemyName = enemySO.EnemyName;
+        enemyDescription = enemySO.EnemyDescription;
+
+        //Floats
+        enemyHealth = enemySO.EnemyHealth;
+        enemySpeed = enemySO.EnemySpeed;
+        enemyDamage = enemySO.EnemyDamage;
+
+    }
 
     //TODO: Probably add an enum for proj types to easily trigger effects
     public void TakeDamage(float damage)
     {
         Debug.Log($"{damage} damage dealt");
+
+        currentHealth -= damage;
     }
 }
