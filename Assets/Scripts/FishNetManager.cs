d8878e3a7e6fc6ba3a954efbce24a275a390175e
Raxadore <liam@nellakir.com.au>
2026-07-28 01:00:09 +1000

    Updating Menu
    Added more ui elements for lobby room.
    
    Made menuUiManager and FishNetManager singletons

--- Assets/Scripts/FishNetManager.cs
+++ Assets/Scripts/FishNetManager.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class FishNetManager : MonoBehaviour
 {
-    
+    public static FishNetManager Instance { get; private set; }
 }
--- Assets/Scripts/Ui/MenuUiManager.cs
+++ Assets/Scripts/Ui/MenuUiManager.cs
@@ -2,11 +2,35 @@ using UnityEngine;
 
 public class MenuUiManager : MonoBehaviour
 {
+    public static MenuUiManager Instance { get; private set; }
+
     [SerializeField]
-    private GameObject browserPanel;
+    private GameObject onlinePanel, browserPanel, lobbyPanel;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void OpenOnlinePanel()
+    {
+        onlinePanel.SetActive(true);
+    }
 
     public void OpenBrowserPanel()
     {
         browserPanel.SetActive(true);
+    }
+
+    public void OpenLobbyPanel()
+    {
+        lobbyPanel.SetActive(true);
     }
 }
