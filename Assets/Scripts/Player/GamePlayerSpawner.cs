83e79659c110eac2d3bf5c8fd8630fd738f8331a
Raxadore <liam@nellakir.com.au>
2026-07-28 14:29:37 +1000

    Players connecting works
    Need to start checking features to see if there are any broken networks

--- Assets/Scripts/Player/GamePlayerSpawner.cs
+++ Assets/Scripts/Player/GamePlayerSpawner.cs
@@ -11,7 +11,7 @@ public class GamePlayerSpawner : MonoBehaviour
     private Transform[] spawnPoints;
 
     [SerializeField]
-    private PlayerRPCHandler playerPrefab;
+    private NetworkObject playerPrefab;
 
     private void Start()
     {
@@ -24,15 +24,14 @@ public class GamePlayerSpawner : MonoBehaviour
     private void SpawnPlayers()
     {
         int spawnIndex = 0;
-        Debug.Log($"Clients connected: {InstanceFinder.ServerManager.Clients.Count}");
+
         foreach (NetworkConnection connection in InstanceFinder.ServerManager.Clients.Values)
         {
-            Debug.Log($"Spawning player for connection: {connection.ClientId}");
             Transform spawnpoint = spawnPoints[spawnIndex % spawnPoints.Length];
 
             NetworkObject player = Instantiate(playerPrefab, spawnpoint.position, spawnpoint.rotation);
 
-            InstanceFinder.ServerManager.Spawn(player, connection);
+            InstanceFinder.ServerManager.Spawn(player, connection, gameObject.scene);
 
             spawnIndex++;
         }
