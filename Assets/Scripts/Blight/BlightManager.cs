1b700e8bd638f19f525774d0ea61d0ab9c67ac88
Raxadore <liam@nellakir.com.au>
2026-08-06 22:40:49 +1000

    Working on blight
    It now spawns in a random direction to the heart crystal, not a straight line.
    Also spawns in a random distance and time.
    
    Set up functions for the blight node and had it implement IInteractable

--- Assets/Scripts/Blight/BlightManager.cs
+++ Assets/Scripts/Blight/BlightManager.cs
@@ -25,6 +25,23 @@ public class BlightManager : NetworkBehaviour
     [SerializeField]
     private BlightNode blightPrefab;
 
+    /// <summary>
+    /// The range on either side of the direction for the node to spawn at. (45 means a total of 90 degree cone)
+    /// </summary>
+    [SerializeField]
+    private float spawnConeRange = 45f;
+
+    /// <summary>
+    /// The distance range the nodes can move forward towards the node by
+    /// </summary>
+    [SerializeField]
+    private float minDistance = 10f, maxDistance = 20f;
+
+    /// <summary>
+    /// The time range that the nodes will spawn at
+    /// </summary>
+    [SerializeField]
+    private float minTime = 1f, maxTime = 1f;
 
     private void Awake()
     {
@@ -38,12 +55,28 @@ public class BlightManager : NetworkBehaviour
         }
     }
 
-    private void Start()
+    public override void OnStartServer()
     {
+        base.OnStartServer();
+
         //Adds all the initial starting points to the forward nodes
         currentForwardNodes.AddRange(startingPoints);
 
-        heartCrystal = FindFirstObjectByType<HeartCrystal>();
+        StartCoroutine(Initialize());
+    }
+
+    /// <summary>
+    /// Coroutine to ensure heart crystal is set before starting blight
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Initialize()
+    {
+        while(heartCrystal == null)
+        {
+            heartCrystal = FindFirstObjectByType<HeartCrystal>();
+
+            yield return null;
+        }
 
         StartSpawnLoop();
     }
@@ -54,6 +87,10 @@ public class BlightManager : NetworkBehaviour
         StartCoroutine(SpawnLoop());
     }
 
+    /// <summary>
+    /// The loop that spawns in the Blight nodes
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator SpawnLoop()
     {
         while(true)
@@ -61,7 +98,9 @@ public class BlightManager : NetworkBehaviour
             Debug.Log("We hit another loop");
             JumpNode();
 
-            yield return new WaitForSeconds(5f);
+            float timer = Random.Range(minTime, maxTime);
+
+            yield return new WaitForSeconds(timer);
 
         }
     }
@@ -77,26 +116,44 @@ public class BlightManager : NetworkBehaviour
         return currentForwardNodes[index];
     }
 
+    /// <summary>
+    /// Jumps the node forward towards the heart crystal
+    /// </summary>
     private void JumpNode()
     {
         Transform jumpNode = SelectJumpPoint();
 
         //Get the direction from the selected node to the crystal
-        Vector3 direction = (heartCrystal.transform.position - jumpNode.position).normalized;
-        Quaternion lookDirection = Quaternion.LookRotation(direction);
+        Vector3 direction = heartCrystal.transform.position - jumpNode.position;
+
         //Reset Y so it doesnt go up
         direction.y = 0;
-        //TODO: Give it a cone range it can spawn in, maybe min and max dist too
+        direction.Normalize();
+
+        //Give it a cone range it can spawn in
+        float rangeOffset = Random.Range(-spawnConeRange, spawnConeRange);
+
+        //Min and max dist 
+        float distOffset = Random.Range(minDistance, maxDistance);
+
+        Vector3 finalDirection = Quaternion.Euler(0f, rangeOffset, 0f) * direction;
 
         //Gets the position the node will spawn at
-        Vector3 targetPos = jumpNode.position + direction * 10;
+        Vector3 targetPos = jumpNode.position + finalDirection * distOffset;
+
+        Quaternion lookDirection = Quaternion.LookRotation(finalDirection);
 
         BlightNode currentNode = Instantiate(blightPrefab, targetPos, lookDirection);
 
-        Debug.Log(currentNode);
-
         //Spawns the object on the server
         Spawn(currentNode.gameObject);
+
+        currentNode.Initialize(jumpNode);
+
+        if(jumpNode.TryGetComponent<BlightNode>(out BlightNode node))
+        {
+            node.SetNextNode(currentNode.transform);
+        }
 
         currentForwardNodes.Remove(jumpNode);
 
--- Assets/Scripts/Blight/BlightNode.cs
+++ Assets/Scripts/Blight/BlightNode.cs
@@ -1,7 +1,59 @@
 using FishNet.Object;
 using UnityEngine;
 
-public class BlightNode : NetworkBehaviour
+public class BlightNode : NetworkBehaviour, IInteractable
 {
     //TODO Add clearing the nodes
+    private Transform nextBlightNode, previousBlightNode;
+
+    public void Initialize(Transform previous)
+    {
+        previousBlightNode = transform;
+    }
+
+    /// <summary>
+    /// Handles what happens when a node is cleared
+    /// </summary>
+    private void BlightCleared()
+    {
+        if (nextBlightNode != null)
+        {
+            //If there is a next node, update it to keep the chain 
+            if(nextBlightNode.TryGetComponent<BlightNode>(out BlightNode node))
+            {
+                node.SetPreviousNode(previousBlightNode);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates the previous node transform
+    /// </summary>
+    /// <param name="previous"></param>
+    public void SetPreviousNode(Transform previous)
+    {
+        previousBlightNode = previous;
+    }
+
+    /// <summary>
+    /// Updates the next node transform
+    /// </summary>
+    /// <param name="next"></param>
+    public void SetNextNode(Transform next)
+    {
+        nextBlightNode = next;
+    }
+
+    
+    public void Interact(NetworkObject player)
+    {
+        //Handles what happens when the player clears the node
+        throw new System.NotImplementedException();
+    }
+
+    
+    public bool CanInteract(NetworkObject player)
+    {
+        throw new System.NotImplementedException();
+    }
 }
