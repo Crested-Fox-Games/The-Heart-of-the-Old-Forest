ade9c80fdc2c750f6a9963abdfe579cf8f174c99
Raxadore <liam@nellakir.com.au>
2026-08-19 18:00:09 +1000

    Reworked towers to have upgrades
    Added dictionaries to the towers so that they can store the upgrades.
    
    Also implemented the upgrade functionality.
    
    Linked this in with the rewards system.
    
    //TODO: Might need to rework this so that all towers of that type get the upgrade, not just the once out at this moment
    //      This might need to be done by having a dictionary of towers, then using a data class to store stat and amount

--- Assets/Scripts/SO/Rewards/TowerStatRewardSO.cs
+++ Assets/Scripts/SO/Rewards/TowerStatRewardSO.cs
@@ -1,10 +1,57 @@
+using FishNet;
+using NUnit.Framework;
 using UnityEngine;
+
+public enum UpgradeType
+{
+    Addition,
+    Multiplacation
+}
 
 [CreateAssetMenu(fileName = "TowerStatRewardSO", menuName = "Rewards/Tower Stat Reward")]
 public class TowerStatRewardSO : RewardSO
 {
+    /// <summary>
+    /// Which tower stat we will be upgrading
+    /// </summary>
+    [Header("--- Reward Info ---")]
+    public TowerStats towerStat;
+
+    /// <summary>
+    /// The scriptable object of the tower that can be upgraded
+    /// </summary>
+    public TowerSO towerSO;
+
+    /// <summary>
+    /// This is whether we are doing additive upgrades or multiplicative upgrades
+    /// </summary>
+    public UpgradeType rewardType;
+
+    /// <summary>
+    /// The amount we are adding or multiplying by
+    /// </summary>
+    public float rewardAmount;
+
     public override void GrantReward(PlayerRef player)
     {
-        throw new System.NotImplementedException();
+        //Checks to ensure we are running this on the server
+        if (!InstanceFinder.IsServerStarted)
+            return;
+
+        //TODO: Might need to rework this so that all towers of that type get the upgrade, not just the once out at this moment
+        //      This might need to be done by having a dictionary of towers, then using a data class to store stat and amount
+        //Gets all active towers and puts them into an array
+        Tower[] towers = FindObjectsByType<Tower>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+
+        //Loops through all towers
+        foreach (Tower tower in towers)
+        {
+            //If the towerSO doesnt match the current tower, skip over this one and continue the loop
+            if (tower.TowerSO != towerSO)
+                continue;
+
+            //Add the upgrade to the tower
+            tower.AddUpgrade(towerStat, rewardType, rewardAmount);
+        }
     }
 }
--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -5,6 +5,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TowerStats
+{
+    Attack,
+    Health,
+    FireRate,
+    Range
+}
+
 public abstract class Tower : NetworkBehaviour
 {
     #region SO Fields
@@ -13,11 +21,13 @@ public abstract class Tower : NetworkBehaviour
 
     protected string towerName, towerDescription;
 
-    protected float attackRange, towerDamage, towerHealth, attackCooldown;
+    protected float attackRange, towerDamage, towerMaxHealth, attackCooldown;
 
     protected GameObject projectile, displayObject;
 
     #endregion
+
+    public TowerSO TowerSO => towerSO;
 
     protected GameObject targetEnemy;
 
@@ -32,18 +42,40 @@ public abstract class Tower : NetworkBehaviour
 
     protected Coroutine attackCoroutine;
 
-    
+    private SyncDictionary<TowerStats, float> towerAdditiveUpgrades = new();
 
-    private void Start()
+    private SyncDictionary<TowerStats, float> towerMultiplicativeUpgrades = new();
+
+    private SphereCollider towerRangeCollider;
+
+    private void Awake()
     {
         InitializeValues();
 
         //Creates the sphere around the tower that they can attack in
-        SphereCollider col = gameObject.AddComponent<SphereCollider>();
-        col.radius = attackRange;
-        col.isTrigger = true;
+        towerRangeCollider = gameObject.AddComponent<SphereCollider>();
+        towerRangeCollider.radius = GetRange();
+        towerRangeCollider.isTrigger = true;
+    }
 
-        currentHealth.Value = towerHealth;
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        IntitializeUpgradeDictionaries();
+
+        currentHealth.Value = towerMaxHealth;
+
+        towerAdditiveUpgrades.OnChange += OnUpgradesChanged;
+        towerMultiplicativeUpgrades.OnChange += OnUpgradesChanged;
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+
+        towerAdditiveUpgrades.OnChange -= OnUpgradesChanged;
+        towerMultiplicativeUpgrades.OnChange -= OnUpgradesChanged;
     }
 
     /// <summary>
@@ -58,7 +90,7 @@ public abstract class Tower : NetworkBehaviour
         //Float
         attackRange = towerSO.AttackRange;
         towerDamage = towerSO.TowerDamage;
-        towerHealth = towerSO.TowerHealth;
+        towerMaxHealth = towerSO.TowerHealth;
         attackCooldown = towerSO.AttackCooldown;
 
         //GameObjects
@@ -66,8 +98,27 @@ public abstract class Tower : NetworkBehaviour
         displayObject = towerSO.DisplayObject;
     }
 
+    /// <summary>
+    /// Initializes the towers upgrade dictionaries
+    /// </summary>
+    private void IntitializeUpgradeDictionaries()
+    {
+        towerAdditiveUpgrades[TowerStats.Attack] = 0f;
+        towerAdditiveUpgrades[TowerStats.FireRate] = 0f;
+        towerAdditiveUpgrades[TowerStats.Health] = 0f;
+        towerAdditiveUpgrades[TowerStats.Range] = 0f;
+
+        towerMultiplicativeUpgrades[TowerStats.Attack] = 1f;
+        towerMultiplicativeUpgrades[TowerStats.FireRate] = 1f;
+        towerMultiplicativeUpgrades[TowerStats.Health] = 1f;
+        towerMultiplicativeUpgrades[TowerStats.Range] = 1f;
+    }
+
     public void TakeDamage(float damage)
     {
+        if (!IsServerStarted)
+            return;
+
         currentHealth.Value -= damage;
 
         if (currentHealth.Value < 0)
@@ -83,5 +134,64 @@ public abstract class Tower : NetworkBehaviour
         }
     }
 
-    
+    /// <summary>
+    /// Adds the upgrades to the dictionary
+    /// </summary>
+    /// <param name="towerStat"></param>
+    /// <param name="upgradeType"></param>
+    /// <param name="amount"></param>
+    public void AddUpgrade(TowerStats towerStat, UpgradeType upgradeType, float amount)
+    {
+        if(upgradeType == UpgradeType.Addition)
+        {
+            towerAdditiveUpgrades[towerStat] += amount;
+        }    
+        else
+        {
+            towerMultiplicativeUpgrades[towerStat] += amount;
+        }
+    }
+
+    private void OnUpgradesChanged(SyncDictionaryOperation op, TowerStats key, float value, bool asServer)
+    {
+        switch(key)
+        {
+            case TowerStats.Range:
+                //Updates the range of the tower
+                towerRangeCollider.radius = GetRange();
+                break;
+
+            case TowerStats.Health:
+
+                //TODO: Factor in losing max health(if possible)
+                //Updates the health of the tower
+                float newMaxHealth = GetHealth();
+
+                float difference = newMaxHealth - towerMaxHealth;
+
+                towerMaxHealth = newMaxHealth;
+                currentHealth.Value += difference;
+                break;
+        }
+    }
+
+    protected float GetDamage()
+    {
+        return (towerDamage + towerAdditiveUpgrades[TowerStats.Attack]) * towerMultiplicativeUpgrades[TowerStats.Attack];
+    }
+
+    protected float GetFireRate()
+    {
+        return (attackCooldown + towerAdditiveUpgrades[TowerStats.FireRate]) * towerMultiplicativeUpgrades[TowerStats.FireRate];
+    }
+
+    protected float GetRange()
+    {
+        return (attackRange + towerAdditiveUpgrades[TowerStats.Range]) * towerMultiplicativeUpgrades[TowerStats.Range];
+    }
+
+    protected float GetHealth()
+    {
+        return (towerDamage + towerAdditiveUpgrades[TowerStats.Health]) * towerMultiplicativeUpgrades[TowerStats.Health];
+    }
 }
