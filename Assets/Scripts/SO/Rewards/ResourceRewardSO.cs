4e92d8ef780e772c8ca08be1608e1015d3bc95c8
Raxadore <liam@nellakir.com.au>
2026-08-19 17:13:27 +1000

    Reward Framework Comments
    Added some comments for clarity
    
    Also added granting player resources

--- Assets/Scripts/SO/Rewards/ResourceRewardSO.cs
+++ Assets/Scripts/SO/Rewards/ResourceRewardSO.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ResourceRewardSO", menuName = "Scriptable Objects/ResourceRewardSO")]
 public class ResourceRewardSO : RewardSO
 {
-    //TODO: Make amount scale if we decide to based on nights cleared (Maybe on blight cleared too?)
-
+    /// <summary>
+    /// The resource granted by the reward system
+    /// </summary>
     public ResourceSO Resource;
+
+    //TODO: Make amount scale if we decide to based on nights cleared (Maybe on blight cleared too?)
+    /// <summary>
+    /// The base amount of resources granted by the reward system
+    /// </summary>
     public int baseAmount;
 
     public override void GrantReward(PlayerRef player)
     {
         //Grant the player resources when this reward is selected
+        player.playerInteraction.AcquireResources(Resource.ResourceType, baseAmount);
     }
 }
--- Assets/Scripts/SO/Rewards/RewardSO.cs
+++ Assets/Scripts/SO/Rewards/RewardSO.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "RewardSO", menuName = "Scriptable Objects/RewardSO")]
 public abstract class RewardSO : ScriptableObject
 {
+    /// <summary>
+    /// The name of the reward granted to the player
+    /// </summary>
     public string RewardName;
+
+    /// <summary>
+    /// The icon for the reward granted to the player
+    /// </summary>
     public Sprite RewardIcon;
 
     /// <summary>
