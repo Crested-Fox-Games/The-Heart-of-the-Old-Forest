dc288c231991adcaf8b121bd7c757005beb32d83
Raxadore <liam@nellakir.com.au>
2026-08-26 23:37:24 +1000

    Updated dev controller
    Reworked it to have multiple maps that we can switch between (not implemented fully)
    
    Added a new keybind for quickly ending the day to test things in later days or end of day events like reward system.
    
    Reworked it so that ~ now opens the menu and 1 and 2 are the dev action keys now.

--- Assets/Scripts/Dev/DevController.cs
+++ Assets/Scripts/Dev/DevController.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,35 +10,107 @@ public class DevController : MonoBehaviour
 
     private InputActionMap devMap;
 
+    private InputActionMap devTimeMap;
+
+    private int currentDevMapIndex = 0;
+
+    private List<InputActionMap> actionMapsList = new List<InputActionMap>();
+
+    [SerializeField]
+    private GameObject devControlPanel;
+
+    private InputAction OpenDevControls;
+
     /// <summary>
-    /// NUMPAD 1 - Skips to night time, for testing night time mechanics
+    /// Skips to night time, for testing night time mechanics
     /// </summary>
     private InputAction skipToNight;
 
+    /// <summary>
+    /// Goes to the end of the day night cycle and starts a new day
+    /// </summary>
+    private InputAction quickEndDay;
+
     private void Awake()
     {
-        devMap = inputActions.FindActionMap("Dev");
+        GetActionMaps();
 
         //Checks if the game is being played in editor
         //NOTE: Might want to add a check for dev build so we can also test in build
     #if UNITY_EDITOR || DEVELOPMENT_BUILD
+        InitializeDevControls();
+    #endif
+    }
+
+    private void GetActionMaps()
+    {
+        devMap = inputActions.FindActionMap("Dev");
+
+        //Controls for time
+        devTimeMap = inputActions.FindActionMap("DevTime");
+        actionMapsList.Add(devTimeMap);
+    }
+
+    private void InitializeDevControls()
+    {
         devMap.Enable();
 
-        skipToNight = devMap.FindAction("SkipToNight");
+        OpenDevControls = devMap.FindAction("OpenDevControls");
+        OpenDevControls.performed += ctx => ActivateDevControls();
+
+        //Find actions for all the action maps(add more functions below)
+        FindDevTimeMapActions();
+    }
+
+    private void ActivateDevControls()
+    {
+        devControlPanel.SetActive(true);
+
+        //Activate default map
+        ActivateDevTimeMapControls();
+
+        //TODO: we might want to have it unsubcribe from this and subscribe to a deactivate later, but might not be worth it
+        OpenDevControls.performed -= ctx => ActivateDevControls();
+    }
+
+    private void FindDevTimeMapActions()
+    {
+        skipToNight = devTimeMap.FindAction("SkipToNight");
+        quickEndDay = devTimeMap.FindAction("QuickEndDay");
+    }
+
+    private void ActivateDevTimeMapControls()
+    {
+        devTimeMap.Enable();
+
         skipToNight.performed += ctx => SkipToNight();
-    #endif
+        quickEndDay.performed += ctx => QuickEndDay();
+    }
+
+    /// <summary>
+    /// This will be added once we have a reason to use other maps
+    /// </summary>
+    private void DeactivateDevTimeMapControls()
+    {
+        devTimeMap.Disable();
+
+        skipToNight.performed -= ctx => SkipToNight();
+        quickEndDay.performed -= ctx => QuickEndDay();
     }
 
     /// <summary>
     /// This #if will only run if the game is being played in the editor
     /// </summary>
     #if UNITY_EDITOR || DEVELOPMENT_BUILD
-    [SerializeField]
-        TimeCycleManager timeCycleManager;
 
         private void SkipToNight()
         {
-            timeCycleManager.SkipToNight();
+            TimeCycleManager.Instance.SkipToNight();
+        }
+
+        private void QuickEndDay()
+        {
+            TimeCycleManager.Instance.QuickEndDay();
         }
     #endif
 
