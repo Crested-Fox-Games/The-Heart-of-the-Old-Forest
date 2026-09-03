e65dacd104fdd0d724db9fe19d1752eef3f2ca46
Raxadore <liam@nellakir.com.au>
2026-09-03 15:54:42 +1000

    Health Bars functional
    Implemented and tested on the badger enemy

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -12,10 +12,10 @@ public class HealthBar : MonoBehaviour
     private GameObject healthBarContainer;
 
     /// <summary>
-    /// The image component that will fill to represent the health bar
+    /// The renderer of the health bar object
     /// </summary>
     [SerializeField]
-    private Image healthBarImage;
+    private Renderer healthBarRenderer;
 
     /// <summary>
     /// The text that will display the health in the form of 'x/x'
@@ -28,17 +28,36 @@ public class HealthBar : MonoBehaviour
     /// </summary>
     private Camera playerCam;
 
+    /// <summary>
+    /// Property blocks are like how a regular script can have different values for different instances
+    /// of the same script, but with materials. This allows us to share the same material across 
+    /// multiple objects without them all changing whenever we update a value.
+    /// </summary>
+    private static readonly int FillProperty = Shader.PropertyToID("_Fill");
+
+    [SerializeField]
+    private float HeightAboveObject = 1f;
+
+    private MaterialPropertyBlock propertyBlock;
+
     private void Start()
     {
+        propertyBlock = new MaterialPropertyBlock();
         playerCam = Camera.main;
     }
 
 
     private void Update()
     {
-        healthBarContainer.transform.rotation = quaternion.LookRotation(playerCam.transform.forward, Vector3.up);
+        Vector3 direction = playerCam.transform.position - healthBarContainer.transform.position;
+        healthBarContainer.transform.rotation = quaternion.LookRotation(direction, Vector3.up);
     }
 
+    /// <summary>
+    /// Function called to update the health bar visuals
+    /// </summary>
+    /// <param name="currentHealth"></param>
+    /// <param name="maxHealth"></param>
     public void TriggerHealthBarUpdate(float currentHealth, float maxHealth)
     {
         float percentage = currentHealth / maxHealth;
@@ -47,19 +66,22 @@ public class HealthBar : MonoBehaviour
         UpdateHealthText(currentHealth, maxHealth);
     }
 
+    /// <summary>
+    /// Updates the actual health bar itself
+    /// </summary>
+    /// <param name="healthPercentage"></param>
     private void UpdateHealthBar(float healthPercentage)
     {
-        if (healthBarImage != null)
-        {
-            healthBarImage.fillAmount = Mathf.Clamp01(healthPercentage);
-        }
-
-        if(healthText != null)
-        {
-            healthText.text = $"";
-        }
+        healthBarRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetFloat(FillProperty, healthPercentage);
+        healthBarRenderer.SetPropertyBlock(propertyBlock);
     }
 
+    /// <summary>
+    /// Updates the health text
+    /// </summary>
+    /// <param name="currentHealth"></param>
+    /// <param name="maxHealth"></param>
     private void UpdateHealthText(float currentHealth, float maxHealth)
     {
         if (healthText != null)
