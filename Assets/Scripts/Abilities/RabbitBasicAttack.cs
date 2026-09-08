779407e5918e9a57e11480665ed62cebdd983560
Raxadore <liam@nellakir.com.au>
2026-09-08 14:50:58 +1000

    Testing ricochet proj to get it functional
    Currently trying to fix weird directional movement

--- Assets/Scripts/Abilities/RabbitBasicAttack.cs
+++ Assets/Scripts/Abilities/RabbitBasicAttack.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class RabbitBasicAttack : Ability
 {
-    private NormalProjectile projectile;
+    private BaseProjectile projectile;
 
     private float damage = 20f;
 
-    public RabbitBasicAttack(PlayerAbilities player, AbilitySO abilityData, NormalProjectile projectile) : base(player, abilityData)
+    public RabbitBasicAttack(PlayerAbilities player, AbilitySO abilityData, BaseProjectile projectile) : base(player, abilityData)
     {
         this.projectile = projectile;
     }
