14394450e8c562abfd03b265acae77a3dd9bf09e
riven-delle <marcus.steenberg1@gmail.com>
2026-09-22 17:39:17 +1000

    Updated comments on TowerPathUpgradeSO

--- Assets/Scripts/SO/TowerUpgradePathSO.cs
+++ Assets/Scripts/SO/TowerUpgradePathSO.cs
@@ -29,13 +29,40 @@ public class TowerUpgradePathSO : ScriptableObject
 
     private TowerUpgradeSO nextRandomUpgrade;
 
+    /// <summary>
+    /// The name displayed for the upgrade path
+    /// </summary>
     public string PathName => pathName;
+
+    /// <summary>
+    /// Maximum amount this unique upgrade can be purchased
+    /// </summary>
     public int MaxUniqueUpgrades => maxUniqueUpgrades;
+
+    /// <summary>
+    /// The unique milestone upgrade for this path
+    /// </summary>
     public TowerUpgradeSO MilestoneUpgrade => milestoneUpgrade;
+
+    /// <summary>
+    /// Pool of random upgrades
+    /// </summary>
     public List<TowerUpgradeSO> RandomUpgradePool => randomUpgradePool;
+
+    /// <summary>
+    /// Number of upgrades between milestone upgrades   
+    /// </summary>
     public int RandomUpgradesBetweenMilestones => randomUpgradesBetweenMilestones;
+
+    /// <summary>
+    /// The next random upgrade in the upgrade pool
+    /// </summary>
     public TowerUpgradeSO NextRandomUpgrade => nextRandomUpgrade;
 
+    /// <summary>
+    /// Returns the next random upgrade in the pool
+    /// </summary>
+    /// <returns></returns>
     public TowerUpgradeSO GetRandomUpgrade()
     {
         if (randomUpgradePool == null || randomUpgradePool.Count == 0)
